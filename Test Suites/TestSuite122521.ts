4b5f4a6a2dcfd9509f3230d9e0930c099c9a9f28
Nguyen Van Minh <minh.nguyen@katalon.com>
2021-12-26 20:13:55 +0700

    first commit

---
+++ Test Suites/TestSuite122521.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite122521</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6a167ed7-3d57-4dda-a873-3cf3d1984c0f</testSuiteGuid>
+   <testCaseLink>
+      <guid>460240c2-e911-4f20-ae6c-39c7f630281b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test.Case.122521a/Folder1.Folder2a/Form122521</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
